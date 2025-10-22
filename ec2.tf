a646178a779817a975933e538b6446008dfbcb10
surendevops75 <surendevops75@gmail.com>
2025-10-22 15:15:16 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "terraform" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami           = "ami-09c813fb71547fc4f"
+
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all-1.id]
 
   tags = {
